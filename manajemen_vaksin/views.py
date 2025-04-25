a5db92d8267ec59416e9c56833dbee9caf3725b7
arishashaista <arishashaistaa@gmail.com>
2025-04-25 19:41:37 +0700

    [ADD] Add views.py

--- manajemen_vaksin/views.py
+++ manajemen_vaksin/views.py
@@ -11,3 +11,18 @@ def update_vaksin_dokter(request):
 
 def delete_vaksin_dokter(request):
     return render(request, "dokter/delete_vaksin_dokter.html")
+
+def vaksin_perawat(request):
+    return render(request, "perawat/vaksin_perawat.html")
+
+def create_vaksin_perawat(request):
+    return render(request, "perawat/create_vaksin_perawat.html")
+
+def update_vaksin_perawat(request):
+    return render(request, "perawat/update_vaksin_perawat.html")
+
+def update_stok(request):
+    return render(request, "perawat/update_stok.html")
+
+def delete_vaksin_perawat(request):
+    return render(request, "perawat/delete_vaksin_perawat.html")
