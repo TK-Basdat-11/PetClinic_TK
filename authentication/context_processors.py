da1fbb4039c52635f87edfb4556c8d1901339fee
farrelreksa <farrelreksa@gmail.com>
2025-05-28 21:14:24 +0700

    feat: add user id to context proc

--- authentication/context_processors.py
+++ authentication/context_processors.py
@@ -3,4 +3,5 @@ def user_role(request):
         "user_role": request.session.get("user_role"),
         "email": request.session.get("email"),
         "is_auth":   request.session.get("is_auth", False),
+        "user_id": request.session.get("user_id"),
     }
